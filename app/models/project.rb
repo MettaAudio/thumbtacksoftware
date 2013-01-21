a2ae82a0c0ed85c7ae14b6aa19cd44a456bf123a
John King <john@mettaaudio.com>
2013-01-21 13:05:54 -0500

    Continuted styling of single and multipage

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :git, :image, :launch_date, :responsibilities, :technology, :title, :url
+
+  def launch_date
+    self[:launch_date].strftime('%B %Y')
+  end
 end
