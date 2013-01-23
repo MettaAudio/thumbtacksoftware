59e377fad2eb9cfbff11ee1e81879ffae33380a6
John King <john@mettaaudio.com>
2013-01-23 13:09:19 -0500

    Add Project#platform to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122043155) do
+ActiveRecord::Schema.define(:version => 20130123160123) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130122043155) do
     t.string   "git"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "platform"
   end
 
 end
