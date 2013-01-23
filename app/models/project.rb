6900bf23c337a179b575d7db98d0c6933318f3c8
John King <john@mettaaudio.com>
2013-01-23 13:22:14 -0500

    Change to single quotes for SQL where clause
    This is my second attempt to fix the PG error we're getting with my
    :html scope. Perhaps PG will be happier with single quotes.

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,6 @@ class Project < ActiveRecord::Base
   scope :ror,     where(:platform => 'Ruby on Rails')
   scope :spree,   where(:platform => 'Spree Commerce / Ruby on Rails')
   scope :shopify, where(:platform => 'Shopify')
-  scope :html,    where('(platform <> "Ruby on Rails" AND platform <> "Spree Commerce / Ruby on Rails" AND platform <> "Shopify") OR platform IS NULL')
+  scope :html,    where("(platform <> 'Ruby on Rails' AND platform <> 'Spree Commerce / Ruby on Rails' AND platform <> 'Shopify') OR platform IS NULL")
 
 end
